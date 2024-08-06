cea36947ead06a450044fe94a776a68bc7cc357d
Kevin Kmetz <kev.kmetz@gmail.com>
2024-08-06 05:24:18 -0700

    Write another test for Lexer - stillHasChars
    Wrote a test for the corresponding function, which passed.
    
    Next up, try to finish the remainging tests for the existing Lexer functions so far.

--- repl/LexerTest.lua
+++ repl/LexerTest.lua
@@ -2,6 +2,11 @@ local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 th
 
 local test_run
 local test_peekLeadingChar
+local test_stillHasChars
+
+
+
+
 
 test_run = function()
    print('Enabling testing: ' .. tostring(___enable_testing_Lexer()))
@@ -9,6 +14,9 @@ test_run = function()
 
    local failureCount = 0
    failureCount = failureCount + test_peekLeadingChar()
+   test_stillHasChars()
+
+   print('Test of Lexer complete.')
    return failureCount
 end
 
@@ -33,6 +41,42 @@ test_peekLeadingChar = function()
    assert(t(s_4) == nil)
    assert(t(s_5) == 'F')
    assert(#t(s_5) == 1)
+
+   io.write(' passed!\n')
+   return 0
+
+end
+
+test_stillHasChars = function()
+
+   io.write('Now running test_stillHasChars...')
+   local t = _t_stillHasChars
+
+   local s_1 = 'A regular string'
+   local r_11, r_12 = t(s_1)
+   local s_2 = 'X'
+   local r_21, r_22 = t(s_2)
+   local s_3 = ''
+   local r_31, r_32 = t(s_3)
+   local s_4 = nil
+   local r_41, r_42 = t(s_4)
+   local s_5 = 'Four score and seven years ago'
+   local r_51, r_52 = t(s_5)
+
+   assert(r_11 == true)
+   assert(r_12 == 'A')
+
+   assert(r_21 == true)
+   assert(r_22 == 'X')
+
+   assert(r_31 == false)
+   assert(r_32 == nil)
+
+   assert(r_41 == false)
+   assert(r_42 == nil)
+
+   assert(r_51 == true)
+   assert(r_52 == 'F')
 
    io.write(' passed!\n')
    return 0
